fc1bca9011d8fa8e5c8db16ce1f25f62194be7bc
Ridhi Shikha <shikha@usc.edu>
2019-06-07 10:14:31 -0400

    mod 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,10 +1,23 @@
 import React,{ Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
+import { timingSafeEqual } from 'crypto';
 
 class QuizQuestion extends Component{
+constructor(props){
+    super(props)
+    this.state = {incorrectAnswer:false}
+}
 handleClick(buttonText){
     if(buttonText == this.props.quiz_question.answer)
-        this.props.showNextQuestionHandler()
+        {
+            this.props.showNextQuestionHandler()
+            this.setState((state => { 
+                return {incorrectAnswer:false}
+            }))
+        }
+    else this.setState ((state => {
+        return {incorrectAnswer: true}
+    }))
 }
 render(){
     return(
@@ -19,6 +32,8 @@ render(){
               })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className='error'> Sorry, that's not right</p>:
+         null}
     </main>
       )
 }

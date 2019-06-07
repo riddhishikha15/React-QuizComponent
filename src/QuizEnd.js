558bc913649658c8070829f414e04eaa829c6b12
Ridhi Shikha <shikha@usc.edu>
2019-06-07 10:33:17 -0400

    mod7

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,16 +1,32 @@
-import React,{ Component } from 'react'
+import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
-class QuizEnd extends Component{
-    handleResetClick(){
-        this.props.resetClickHandler()
+let quizData = require('./quiz_data.json')
+class Quiz extends Component{
+    constructor(props){
+        super(props)
+        this.state = {quiz_position:1}
     }
-    render(){
-        return(
-            <div>
-                <p>Thanks for playing!</p>
-                <a href='#' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
-          </div>
-        )
+handleResetClick(){
+        this.setState({quiz_position: 1})
+      
     }
+showNextQuestion(){
+    this.setState((state)=>{
+        return {quiz_position: this.state.quiz_position + 1}
+    }) 
+    }
+render(){
+    const isQuizEnd = ((this.state.quiz_position-1)==quizData.quiz_questions.length)
+    return (
+        <div>
+            {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>:
+            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
+        </div>
+    )
+        
 }
-export default QuizEnd
+
+}
+export default Quiz
